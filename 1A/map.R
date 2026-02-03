1dc09bb1e0bf5ffaf19f724679fb2c44cdec33b4
yiliao1022 <34287238+yiliao1022@users.noreply.github.com>
2026-02-03 17:30:03 +0800

    Refactor map generation using rnaturalearth
    Updated the map generation process to use the rnaturalearth package for land data and improved the ggplot configuration for better visualization.

--- 1A/map.R
+++ 1A/map.R
@@ -1,31 +1,60 @@
+library(sf)
 library(ggplot2)
-library(sp)
-library(tmap)
+library(rnaturalearth)
+library(rnaturalearthdata)
 
-# world sampled  individuals x y coordinate
-mydata <- read.table("./input/map.txt", header=TRUE, sep="\t")
-visit.x <- mydata$x
-visit.y <- mydata$y
-mp<-NULL
+mydata <- read.table("./input/map.txt",header = TRUE, sep = "\t")
 
-# world map border object
-mapworld <- borders("world", fill="white", xlim=c(-200,200), colour="black", size=0.2)
-# world map
-mp <- ggplot() + mapworld + ylim(-60,90)
-# world map with sample dots
-mp2<- mp + geom_point(size = 1, 
-  aes(x=visit.x,y=visit.y,color=mydata$population1))+
-          scale_color_manual(breaks = c("C1","C2","C3"),
-                     values = c("#EF6F6F","#495D90","#37A95A"))
-# world map with blue background
-mp3<- mp2 +theme(panel.grid.major =element_blank(), 
-                     panel.grid.minor = element_blank(),
-                     panel.background = element_rect(fill = '#AED8E6', colour = 'black'),
-                     panel.border = element_blank(),
-                 legend.background = element_blank(),
-                 legend.key = element_blank(),
-                 legend.position = c(0.13, 0.10))
-# output
-mp3
+land <- ne_download(
+  scale = 50,
+  type = "land",
+  category = "physical",
+  returnclass = "sf"
+)
 
-ggsave(mp3, filename="map.pdf", width=10, height=6, dpi=300)
+lat_lines <- c(80, 60, 40, 20, 0, -20, -40, -60)
+lon_lines <- c(-160, -120, -80, -40, 0, 40, 80, 120, 160)
+
+mp <- ggplot() +
+  geom_sf(data = land,
+          fill = "#F2E6C9",
+          color = NA) + 
+  geom_hline(yintercept = lat_lines,
+             linetype = "dashed",
+             color = "grey",
+             size = 0.25) +
+  geom_vline(xintercept = lon_lines,
+             linetype = "dashed",
+             color = "grey",
+             size = 0.25) +
+  geom_point(
+    data = mydata,
+    aes(x = x, y = y,
+        color = population1,
+        shape = population1),
+    size = 1.5
+  ) +
+  scale_color_manual(
+    breaks = c("out", "C1", "C2", "C3", "C4"),
+    values = c("black", "#EF6F6F", "#495D90","#37A95A", "#B083B5")
+  ) +
+  scale_shape_manual(
+    breaks = c("out", "C1", "C2", "C3", "C4"),
+    values = c(16, 16, 15, 18, 17)
+  ) +
+  coord_sf(
+    xlim = c(-180, 180),
+    ylim = c(-60, 90),
+    expand = FALSE
+  ) +
+  theme(
+    panel.background = element_rect(fill = "#B5E1FF", colour = "black"),
+    panel.grid = element_blank(),
+    panel.border = element_blank(),
+    legend.background = element_blank(),
+    legend.key = element_blank(),
+    legend.position = c(0.13, 0.10)
+  )
+
+mp
+ggsave("map.pdf",mp,width = 10,height = 6,dpi = 300)
